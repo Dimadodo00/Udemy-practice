6310fbee79147fdedf6f38d8947b52011b4eaa8d
Dmytro <dimadodo00@gmail.com>
2021-11-25 08:09:29 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("18+?", "18");
+//console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('How old are you?', '');
+answers[2] = prompt('You married?', '');
+answers[3] = prompt('Do you like cyling?', '');
+
+document.write(answers);
